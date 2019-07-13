486372084033c7c6603f2b04bbe3e29d4cbbe40b
DannyGersh <51251017+DannyGersh@users.noreply.github.com>
2019-07-13 05:24:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,245 @@
+//world surface area
+{
+    var options = {
+        chart: {
+            width: 400,
+            type: 'pie',
+        },
+        title: {
+            text: "Land surface (million km2)"
+        },
+        tooltip: {
+            enabled: true,
+            style: {
+                fontSize: '18px',
+                fontFamily: undefined
+            }
+        },
+        labels: ['"free" land', 'water', 'cropland', 'pastureland'],
+        series: [98.740, 361.132, 16.700, 33.500],
+        responsive: [{
+            breakpoint: 480,
+            options: {
+                chart: {
+                    width: 200
+                },
+                legend: {
+                    position: 'bottom'
+                }
+            }
+        }]
+    }
+    var chart = new ApexCharts(
+        document.querySelector("#world_surface_area"),
+        options
+    );
+    chart.render();
+}
+
+//energy_by_ghg_co2_eq_chart
+{
+    var options = {
+        chart: {
+            type: 'bar',
+        },
+        plotOptions: {
+            bar: {
+                horizontal: true,
+            }
+        },
+        tooltip: {
+            fixed: {
+                enabled: true,
+                offsetX: -10,
+                offsetY: 300
+            },
+            custom: function ({ series, seriesIndex, dataPointIndex, w }) {
+                return '<div class="tooltip">'
+                    +
+                    '<span style="color:  '
+                    +
+                    w.globals.colors[seriesIndex]
+                    +
+                    '">'
+                    +
+                    w.config.xaxis.categories[dataPointIndex]
+                    +
+                    ': '
+                    +
+                    series[seriesIndex][dataPointIndex]
+                    +
+                    '</span>'
+            },
+        },
+        dataLabels: {
+            enabled: false
+        },
+
+        series: [{
+            name: 'Min',
+            data: [740, 620, 410, 130, 18, 26, 6.0, 8.8, 1.0, 8.0, 3.7, 7.0]
+        }, {
+            name: 'Median',
+            data: [820, 740, 490, 230, 48, 41, 38, 27, 24, 12, 12, 11]
+        }, {
+            name: 'Max',
+                data: [910, 890, 650, 420, 180, 60, 79, 63, 2200, 35, 110, 56]
+        }],
+        xaxis: {
+            categories: ['Coal – PC', 'Biomass – Cofiring with coal',
+                'Gas – combined cycle', 'Biomass – Dedicated',
+                'Solar PV – Utility scale', 'Solar PV – rooftop',
+                'Geothermal', 'Concentrated solar power', 'Hydropower',
+                'Wind Offshore', 'Nuclear', 'Wind Onshore'],
+        }
+    }
+
+    var chart = new ApexCharts(
+        document.querySelector("#energy_by_ghg_co2_eq_chart"),
+        options
+    );
+
+    chart.render();
+}
+
+//Energy_by_land_chart
+{
+    var options = {
+        chart: {
+            type: 'bar',
+        },
+        plotOptions: {
+            bar: {
+                horizontal: true,
+            }
+        },
+        legend: {
+            show: true
+        },
+        tooltip: {
+            custom: function ({ series, seriesIndex, dataPointIndex, w }) {
+                return '<div class="tooltip">'
+                    +
+                    '<span style="color:  '
+                    +
+                    w.globals.colors[seriesIndex]
+                    +
+                    '">'
+                    +
+                    w.config.xaxis.categories[dataPointIndex]
+                    +
+                    ': '
+                    +
+                    series[seriesIndex][dataPointIndex]
+                    +
+                    '</span>'
+            },
+        },
+        dataLabels: {
+            enabled: false
+        },
+
+        series: [{
+            name: 'Acres per Megawatt Produced',
+            data: [12.21,12.41,12.71,43.50,70.64,315.22]
+        }],
+        xaxis: {
+            categories: ['Coal', 'Natural Gas', 'Nuclear', 'Solar', 'Wind', 'Hydro'],
+        }
+    }
+
+    var chart = new ApexCharts(
+        document.querySelector("#Energy_by_land_chart"),
+        options
+    );
+
+    chart.render();
+}
+
+//lazy load
+{
+    (function () {
+        var lazyLoadInstances = [];
+        function logElementEvent(eventName, element) {
+            console.log(
+                Date.now(),
+                eventName,
+                element.getAttribute("data-src")
+            );
+        }
+        var callback_enter = function (element) {
+            // Instantiate a new LazyLoad on the element that entered
+            var oneLL = new LazyLoad({
+                container: element
+            });
+            // Optionally push it in the lazyLoadInstances
+            // array to keep track of the instances
+            lazyLoadInstances.push(oneLL);
+            logElementEvent(" ENTERED", element);
+        };
+        var callback_exit = function (element) {
+            logElementEvent(" EXITED", element);
+        };
+        var callback_reveal = function (element) {
+            logElementEvent(" REVEALED", element);
+        };
+        var callback_loaded = function (element) {
+            logElementEvent(" LOADED", element);
+        };
+        var callback_error = function (element) {
+            logElementEvent(" ERROR", element);
+            element.src =
+                "https://via.placeholder.com/440x560/?text=Error+Placeholder";
+        };
+        var callback_finish = function () {
+            logElementEvent(" FINISHED", document.documentElement);
+        };
+        // The "lazyLazy" instance of lazyload is used
+        // to check when the .horzContainer divs enter the viewport
+        var lazyLazy = new LazyLoad({
+            elements_selector: ".horzContainer",
+            // Assign the callbacks defined above
+            callback_enter: callback_enter,
+            callback_exit: callback_exit,
+            callback_reveal: callback_reveal,
+            callback_loaded: callback_loaded,
+            callback_error: callback_error,
+            callback_finish: callback_finish
+        });
+        ll = new LazyLoad({
+            elements_selector: ".lazy",
+            // Assign the callbacks defined above
+            callback_enter: callback_enter,
+            callback_exit: callback_exit,
+            callback_reveal: callback_reveal,
+            callback_loaded: callback_loaded,
+            callback_error: callback_error,
+            callback_finish: callback_finish
+        });
+    })();
+}
+
+//button
+{
+    b_func('Energy_Life_cycle_assessment');
+    b_func('Water');
+    b_func('Basic information');
+    b_func('World_energy_by_source');
+    b_func('Energy');
+    b_func('Atmosphere');
+    b_func('Land');
+    b_func('Biodiversity');
+    b_func('land_energy');
+    function b_func(q) {
+        var x = document.getElementById(q);
+        if (x.style.display === "none") {
+            x.style.display = "flex";
+        } else {
+            x.style.display = "none";
+        }
+        if (x.id === 'land_energy') {
+            energy_land_chart_draw();
+        }
+    }
+}
+
